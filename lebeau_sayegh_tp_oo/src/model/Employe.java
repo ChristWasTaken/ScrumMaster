fb0dcc3207ff5e7781388148965a5950890ab52b
Yann <y.lebeau84@gmail.com>
2022-03-22 10:34:07 -0400

    Revert "4.3.1"
    This reverts commit 10329a8e399aab3d93e4870a6ebd515d02f00fdc.

--- lebeau_sayegh_tp_oo/src/model/Employe.java
+++ lebeau_sayegh_tp_oo/src/model/Employe.java
@@ -1,12 +1,15 @@
 package model;
 
 public class Employe {
+    private static int employeID;
     private String nom, prenom, poste;
 
-    public Employe(){
+    public Employe() {
+        employeID++;
     }
 
     public Employe(String nom, String prenom, String poste) {
+        employeID++;
         this.nom = nom;
         this.prenom = prenom;
         this.poste = poste;
