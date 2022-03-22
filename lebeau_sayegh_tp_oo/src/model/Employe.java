10329a8e399aab3d93e4870a6ebd515d02f00fdc
Yann <y.lebeau84@gmail.com>
2022-03-22 10:30:16 -0400

    4.3.1

--- lebeau_sayegh_tp_oo/src/model/Employe.java
+++ lebeau_sayegh_tp_oo/src/model/Employe.java
@@ -1,15 +1,12 @@
 package model;
 
 public class Employe {
-    private static int employeID;
     private String nom, prenom, poste;
 
-    public Employe() {
-        employeID++;
+    public Employe(){
     }
 
     public Employe(String nom, String prenom, String poste) {
-        employeID++;
         this.nom = nom;
         this.prenom = prenom;
         this.poste = poste;
